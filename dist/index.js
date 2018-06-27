ad8d2a3470561aad0232664e1f3e101aa4583dc2
islishude <lishude@aliyun.com>
2018-06-27 16:29:16 +0800

    fix: fix env

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Redis = require("ioredis");
 const port = Number.parseInt(process.env.redisPort) || 6379;
-const host = process.env.redisIp || "127.0.0.1";
+const host = process.env.redisHost || process.env.redisIp || "127.0.0.1";
 const password = process.env.redisPassword || "";
 exports.RedisClient = new Redis(port, host, { password });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as Redis from "ioredis";
 
 const port = Number.parseInt(process.env.redisPort) || 6379;
-const host = process.env.redisIp || "127.0.0.1";
+const host = process.env.redisHost || process.env.redisIp || "127.0.0.1";
 const password = process.env.redisPassword || "";
 
 export const RedisClient: Redis.Redis = new Redis(port, host, { password });
