4fd5c9d29c84921d5f72a04d6412e547c16e3405
islishude <lishude@aliyun.com>
2018-07-03 12:28:13 +0800

    feat: add connect logger

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const console_1 = require("console");
 const Redis = require("ioredis");
 const port = Number.parseInt(process.env.redisPort) || 6379;
-const host = process.env.redisHost || process.env.redisIp || "127.0.0.1";
+const host = process.env.redisIp || "127.0.0.1";
 const password = process.env.redisPassword || "";
-exports.RedisClient = new Redis(port, host, { password });
+exports.RedisClient = new Redis(port, host, {
+    keepAlive: 1,
+    password
+});
+exports.RedisClient.on("connect", () => {
+    console_1.log("Redis 连接成功 %s:%s", host, port);
+});
+exports.RedisClient.on("error", err => {
+    console_1.log("Redis 连接失败 %s:%s，错误原因：%s", err.address, err.port, err.message);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import { log } from "console";
 import * as Redis from "ioredis";
 
 const port = Number.parseInt(process.env.redisPort) || 6379;
-const host = process.env.redisHost || process.env.redisIp || "127.0.0.1";
+const host = process.env.redisIp || "127.0.0.1";
 const password = process.env.redisPassword || "";
 
-export const RedisClient: Redis.Redis = new Redis(port, host, { password });
+export const RedisClient: Redis.Redis = new Redis(port, host, {
+  keepAlive: 1,
+  password
+});
+
+RedisClient.on("connect", () => {
+  log("Redis 连接成功 %s:%s", host, port);
+});
+
+RedisClient.on("error", err => {
+  log("Redis 连接失败 %s:%s，错误原因：%s", err.address, err.port, err.message);
+});
